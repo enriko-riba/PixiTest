4c99e3a435ec940655360bcd729ed689811df0d3
eriba <Enriko.Riba@span.eu>
2017-01-10 16:42:49 +0100

    .

---
+++ Web/Server/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PixiTest.Controllers
+{
+    public class UserController : ApiController
+    {
+        [Route("api/user/login")]
+        public void Login(string email, string pwd)
+        {
+
+        }
+    }
+}
