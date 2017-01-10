af689025c75e1c5563b6694a49e22bc023aa7672
eriba <Enriko.Riba@span.eu>
2017-01-10 16:42:49 +0100

    .

---
+++ Web/Server/Models/LoginResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace PixiTest.Server.Models
+{
+    public class LoginResponse
+    {
+        public int UserId { get; set; }
+
+        public int LastLevel { get; set; }
+
+        public int Gold { get; set; }        
+    }
+}
