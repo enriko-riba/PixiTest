d76641f4c7415f98b80e013f301536c7ddb91f0d
eriba <Enriko.Riba@span.eu>
2017-02-01 15:47:15 +0100

    cleanup, assets, added initial Mob support

---
+++ Web/app/Game/Mob.ts
@@ -0,0 +1,32 @@
+﻿import { AnimatedSprite, AnimationSequence } from "app/_engine/AnimatedSprite";
+
+let FRAME_SIZE: number = 48;
+
+/**
+ * Represents a monster entity
+ */
+export class Mob extends AnimatedSprite {
+
+    private attributes: number[];
+
+    constructor(private textureName: string) {
+        super();
+
+        this.addAnimations(new AnimationSequence("left", textureName, [0, 1, 2], FRAME_SIZE, FRAME_SIZE));
+        this.addAnimations(new AnimationSequence("right", textureName, [3, 4, 5], FRAME_SIZE, FRAME_SIZE));
+        this.addAnimations(new AnimationSequence("latk", textureName, [6, 7, 8], FRAME_SIZE, FRAME_SIZE));
+        this.addAnimations(new AnimationSequence("ratk", textureName, [9, 10 , 11], FRAME_SIZE, FRAME_SIZE));
+        this.PlayAnimation("latk", 2);
+    }
+
+
+    public set Attributes(values: number[]) {
+        this.attributes = values;
+    }
+    public get Attributes(): number[] {
+        return this.attributes;
+    }
+
+    public update = (dt: number) => {
+    }
+}
